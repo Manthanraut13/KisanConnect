ecdeaa4097cfb759aa33b5489674565f32850bdd
khamankartukesh <tukeshkhamankar@gmail.com>
2026-09-05 21:50:45 +0530

    feat: add payment service, controller, routes with razorpay integration - tested

--- backend/src/app.js
+++ backend/src/app.js
@@ -34,6 +34,7 @@ app.use('/api/webhooks', require('./routes/webhook.routes'));
 app.use('/api/listings', require('./routes/listing.routes'));
 app.use('/api/cart', require('./routes/cart.routes'));
 app.use('/api/orders', require('./routes/order.routes'));
+app.use('/api/payments', require('./routes/payment.routes'));
 app.use(errorHandler);
 
 module.exports = app;
